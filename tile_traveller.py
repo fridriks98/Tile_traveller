e6dcde2eb4d68443703290a6d6676a383a30fbde
Fridrik Snaer <frikkifc@gmail.com>
2018-09-18 14:25:15 +0000

    Updated Files

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,13 +1,48 @@
 x_pos = 1
 y_pos = 1
 
-word_list = ['e', 'E', 'n', 'N', 's', 'S', 'w', 'W']
-Direction_list = ["(E)ast", "(N)orth",'(S)outh', "(W)est"]
+E = "(E)ast"
+N = "(N)orth"
+S = "(S)outh"
+W = "(W)est"
 
-direction = input("Direction: ")
-
-if direction in word_list:
-    if y_pos == 1:
-        print("You can travel: {}".format(Direction_list[1]))
-
-
+while True:
+    Bool = True
+    if x_pos == 3 and y_pos == 1:
+        print("Victory!")
+        break
+    elif x_pos == 3 and y_pos == 2:
+        print("You can travel: {} or {}.".format(N,S))
+    elif x_pos == 3 and y_pos == 3:
+        print("You can travel: {} or {}.".format(S,W))
+    elif x_pos == 2 and y_pos == 1:
+        print("You can travel: {}.".format(N))
+    elif x_pos == 2 and y_pos == 2:
+        print("You can travel: {} or {}.".format(S,W))
+    elif x_pos == 2 and y_pos == 3:
+        print("You can travel: {} or {}.".format(E,W))
+    elif x_pos == 1 and y_pos == 1:          
+        print("You can travel: {}.".format(N))
+    elif x_pos == 1 and y_pos == 2:
+        print("You can travel: {} or {} or {}.".format(E,N,S))
+    elif x_pos == 1 and y_pos == 3:
+        print("You can travel: {} or {}.".format(E,S))
+    
+    while Bool == True:
+        direction = input("Direction: ").lower()
+        if direction == 'e' and x_pos != 3:
+            x_pos += 1
+            Bool = False
+        elif direction == 'n' and y_pos != 3:
+            y_pos += 1
+            Bool = False
+        elif direction == 's' and y_pos != 1:
+            y_pos -= 1
+            Bool = False
+        elif direction == 'w' and x_pos != 1:
+            x_pos -= 1
+            Bool = False
+        if Bool == True:
+            print("Not a valid direction!")
+             
+        
